872206b370fa733851a3db2297d480a336667456
Mirwais Farahi <waisfarahi@gmail.com>
2022-07-30 23:38:02 +0500

    Add BookCard.js File

---
+++ src/components/BookCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function BookCard() {
+  return (
+    <div>
+      <li>
+
+        Mirwais
+        :
+        Java Programming
+      </li>
+      <button type="button"> Remove</button>
+    </div>
+  );
+}
+
+export default BookCard;
