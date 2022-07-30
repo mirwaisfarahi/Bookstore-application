9e13ab059613b998ede9faf135747854609dc00f
Mirwais Farahi <waisfarahi@gmail.com>
2022-07-31 01:04:56 +0500

    Edit App.js File

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import './App.css';
 import BooksList from './components/BooksList';
 import Catagories from './components/Categories';
 import Navigation from './components/Navigation';
-import AddBook from './components/AddBook';
 
 function App() {
   return (
@@ -18,7 +17,6 @@ function App() {
         <Routes>
           <Route exact path="/" element={<BooksList />} />
           <Route exact path="/Catagories" element={<Catagories />} />
-          <Route exact path="/AddBook" element={<AddBook />} />
         </Routes>
       </BrowserRouter>
     </div>
